5f026a0810998a91eaf40dda9269eaf83567f5fc
Giso Grimm <giso.grimm@vegri.net>
2020-05-20 11:22:51 +0200

    allow multiple devices per user

--- lobby/www/index.php
+++ lobby/www/index.php
@@ -2,7 +2,7 @@
 
 include '../php/ovbox.inc';
 
-$user = '';
+$user = getenv('ovboxuser');
 
 if (isset($_SERVER['REMOTE_USER']))
     $user = $_SERVER['REMOTE_USER'];
@@ -10,25 +10,97 @@ if (isset($_SERVER['REMOTE_USER']))
 if( isset($_SERVER['REDIRECT_REMOTE_USER']))
     $user = $_SERVER['REDIRECT_REMOTE_USER'];
 
+// require a valid user:
+if( empty($user) )
+    die();
+
+// device update:
 if ($user == 'device') {
     $device = '';
     if( isset($_GET['dev']) )
         $device = $_GET['dev'];
-    $devhash = '';
-    if( isset($_GET['hash']) )
-        $devhash = $_GET['hash'];
-    get_tascar_cfg( $device, $devhash );
+    if( !empty($device) ){
+        $devhash = '';
+        if( isset($_GET['hash']) )
+            $devhash = $_GET['hash'];
+        get_tascar_cfg( $device, $devhash );
+        // touch device file:
+        touch( '../db/' . $device . '.device' );
+    }
     die();
 }
 
-if( empty($user) )
+// admin page:
+if( $user == 'admin' ){
+    if( isset($_GET['setdevowner']) )
+        modify_device_prop( $_GET['setdevowner'], 'owner', $_GET['owner'] );
+    if( isset($_GET['setdevlabel']) )
+        modify_device_prop( $_GET['setdevlabel'], 'label', $_GET['label'] );
+    if( isset($_GET['rmdevice']) )
+        rm_device( $_GET['rmdevice'] );
+    if( isset($_GET['setroomlabel']) )
+        modify_room_prop( $_GET['setroomlabel'], 'name', $_GET['label'] );
+    if( isset($_GET['setroomreverb']) ){
+        modify_room_prop( $_GET['setroomreverb'], 'size', $_GET['size'] );
+        modify_room_prop( $_GET['setroomreverb'], 'rvbgain', $_GET['rvbgain'] );
+        modify_room_prop( $_GET['setroomreverb'], 'rvbdamp', $_GET['rvbdamp'] );
+    }
+    if( isset($_GET['rmroom']) )
+        rm_room( $_GET['rmroom'] );
+    print_head( $user );
+    echo '<form><button>Refresh</button></form>';
+    $devs = list_devices();
+    echo "<h2>Devices:</h2><table>\n";
+    echo "<tr><th>device</th><th>age</th><th>owner</th><th>label</th></tr>\n";
+    foreach( $devs as $dev ){
+        $dprop = get_device_prop( $dev['dev'] );
+        echo '<tr><td>' .
+            $dev['dev'] . '</td><td>' .
+            $dev['age'] . '</td><td>' .
+            '<form><input type="hidden" name="setdevowner" value="'.$dev['dev'].'"><input type="text" name="owner" pattern="[a-zA-Z0-9]*" value="'.$dprop['owner'].'"><button>Save</button></form>' .
+            '</td><td>' .
+            '<form><input type="hidden" name="setdevlabel" value="'.$dev['dev'].'"><input type="text" name="label" pattern="[a-zA-Z0-9]*" value="'.$dprop['label'].'"><button>Save</button></form>' .
+            '</td><td>' .
+            '<form><input type="hidden" name="rmdevice" value="'.$dev['dev'].'"><button>Delete</button></form>' .
+            '</td></tr>' . "\n"; 
+    }
+    echo "</table>\n";
+    $rooms = list_rooms();
+    echo "<h2>Rooms:</h2><table>\n";
+    echo "<tr><th>room</th><th>age</th><th>label</th><th>reverb (size [m x m x m]/gain [dB]/damping)</th></tr>\n";
+    foreach( $rooms as $room ){
+        $dprop = get_room_prop( $room['room'] );
+        echo '<tr><td>' .
+            $room['room'] . '</td><td>' .
+            $room['age'] . '</td><td>' .
+            '<form><input type="hidden" name="setroomlabel" value="'.$room['room'].'"><input type="text" name="label" pattern="[a-zA-Z0-9]*" value="'.$dprop['name'].'"><button>Save</button></form>' .
+            '</td><td>' .
+            '<form>' .
+            '<input type="hidden" name="setroomreverb" value="'.$room['room'].'">'.
+            '<input type="text" name="size" pattern="[ 0-9\.]*" value="'.$dprop['size'].'">'.
+            '<input type="number" name="rvbgain" min="-20" max="0" step="0.1" value="'.$dprop['rvbgain'].'">'.
+            '<input type="number" name="rvbdamp" min="0" max="1" step="0.01" value="'.$dprop['rvbdamp'].'">'.
+            '<button>Save</button>' .
+            '</form>' .
+            '</td><td>' .
+            '<form><input type="hidden" name="rmroom" value="'.$room['room'].'"><button>Delete</button></form>' .
+            '</td></tr>' . "\n"; 
+    }
+    echo "</table>\n";
+    print_foot();
     die();
+}
 
+// room update:
 if ($user == 'room') {
     if( isset($_GET['port']) && isset($_GET['name']) && isset($_GET['pin']) ) {
         update_room( $_SERVER['REMOTE_ADDR'], $_GET['port'], $_GET['name'], $_GET['pin'] );
     }
     die();
+}
+
+if( isset($_GET['devselect']) ){
+    select_userdev( $user, $_GET['devselect'] );
 }
 
 $device = get_device( $user );
@@ -45,9 +117,7 @@ if( isset($_GET['swapdev']) ){
 }
 
 if( isset($_GET['setdevprop']) ){
-    error_log('setdevprop');
     if( !empty( $device ) ){
-        error_log($device);
         $prop = get_device_prop( $device );
         $prop['reverb'] = isset($_GET['reverb']);
         $prop['peer2peer'] = isset($_GET['peer2peer']);
@@ -55,22 +125,26 @@ if( isset($_GET['setdevprop']) ){
             $prop['jittersend'] = $_GET['jittersend'];
         if( isset($_GET['jitterreceive']) )
             $prop['jitterreceive'] = $_GET['jitterreceive'];
+        if( isset($_GET['label']) )
+            $prop['label'] = $_GET['label'];
         set_device_prop( $device, $prop );
     }
 }
 
 print_head( $user );
     
-//html_user_selector( $user );
 
 if ( empty( $device ) ) {
     echo "<p>You are logged in as user {$user}. No device linked to this account.</p>";
-    echo '<p><a href="http://:@' . $_SERVER['HTTP_HOST'] . '">logout</a></p>';
+    html_device_selector( $user, $device );
 } else {
     echo "<p>You are logged in as user <b>{$user}</b> with device <b>{$device}</b>.</p>";
+    html_device_selector( $user, $device );
     $devprop = get_device_prop( $device );
     echo '<form class="devprop"><div class="devproptitle">Device properties:</div>' . "\n";
     // device properties:
+    echo '<label for="label">device label: </label><br>';
+    echo '<input id="label" name="label" type="text" value="'.$devprop['label'].'" pattern="[a-zA-Z0-9]*"><br>' . "\n";
     echo '<label for="jittersend">sender jitter (affects buffer length of others): </label><br>';
     echo '<input id="jittersend" name="jittersend" type="number" min="2" max="30" value="'.$devprop['jittersend'].'"><br>' . "\n";
     echo '<label for="jitterreceive">receiver jitter (affects your own buffer length): </label><br>';
@@ -102,8 +176,7 @@ if ( empty( $device ) ) {
             $roomdev = get_devices_in_room( $room );
             ksort($roomdev);
             foreach( $roomdev as $chair => $dev ){
-                $devuser = get_device_user($dev);
-                if ( $devuser == $user ){
+                if ( $dev == $device ){
                     echo '<b>';
                 }else{
                     if( $room == $devprop['room'] ) {
@@ -111,8 +184,9 @@ if ( empty( $device ) ) {
                     }
                 }
                 //echo htmlspecialchars($devuser) . ' ' . $chair;
-                echo htmlspecialchars($devuser);
-                if ( $devuser == $user ){
+                $devuser = get_device_prop($dev);
+                echo htmlspecialchars($devuser['label']);
+                if ( $dev == $device ){
                     echo '</b>';
                 }else{
                     if( $room == $devprop['room'] ) {
