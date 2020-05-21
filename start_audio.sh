8eedb6f07bc832307e2a3f734a428289b2d95b4f
Giso Grimm <giso.grimm@vegri.net>
2020-05-21 13:02:33 +0200

    do not wait for sound card

--- start_audio.sh
+++ start_audio.sh
@@ -2,9 +2,9 @@
 JACKCMD="jackd --sync -P 40 -d alsa -d hw:1 -r 48000 -p 96 -n 2"
 . get_boxname.sh
 killall jackd zita-j2n zita-n2j tascar_cli
-while ! (cat /proc/asound/cards | grep -q -e US2x2); do
-    sleep 4
-done
+#while ! (cat /proc/asound/cards | grep -q -e US2x2); do
+#    sleep 4
+#done
 sleep 2
 $JACKCMD &
 sleep 4
