7cc4449f0c46a1160da2ce553850b40bc7c20f56
Giso Grimm <giso.grimm@vegri.net>
2020-05-21 13:08:03 +0200

    fix library path

--- start_all.sh
+++ start_all.sh
@@ -3,6 +3,7 @@ CODIR=$(dirname `which $0`)
 (
     echo $CODIR
     cd $CODIR
+    export LD_LIBRARY_PATH=/usr/local/lib
     # make sure this is the first step, so we can fix anything later remotely:
     git pull || (sleep 20 ; git pull)
     # compile binary tools:
