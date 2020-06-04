bfbf012259edc64f44cc337e02a41a7883fdaea1
Giso Grimm <giso.grimm@vegri.net>
2020-06-04 21:41:11 +0200

    fix installer script

--- installovbox.sh
+++ installovbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install dependencies:
-sudo apt install git zita-njbridge jackd2 liblo-dev nodejs libcurl4-openssl-dev build-essential libxml++2.6-dev libwebkit2gtk-4.0-dev $
+sudo apt install git zita-njbridge jackd2 liblo-dev nodejs libcurl4-openssl-dev build-essential libxml++2.6-dev libwebkit2gtk-4.0-dev libasound2-dev libboost-all-dev libcairomm-1.0-dev libeigen3-dev libfftw3-dev libfftw3-double3 libfftw3-single3 libgsl-dev libgtkmm-3.0-dev libgtksourceviewmm-3.0-dev libjack-jackd2-dev libltc-dev libmatio-dev libsndfile1-dev imagemagick
 
 # clone and install TASCAR acoustic simulator repository:
 test -e tascar || git clone https://github.com/gisogrimm/tascar.git
@@ -32,7 +32,6 @@ sudo sed -i "s/127.0.1.1.*raspberry/127.0.1.1\tovbox/g" /etc/hosts
 
 # clone ovbox repo if not yet available:
 sudo su -l ov -c "test -e ovbox || git clone http://github.com/gisogrimm/ovbox"
-
 sudo su -l ov -c "make -C ovbox"
 
 sudo raspi-config nonint enable_overlayfs
