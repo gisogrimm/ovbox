770e50f26c85b2ef187966ae0f615bc8cc399471
Giso Grimm <giso.grimm@vegri.net>
2020-08-31 16:50:28 +0200

    correct URL

--- start_audio.sh
+++ start_audio.sh
@@ -4,6 +4,6 @@ export JACK_NO_AUDIO_RESERVATION=1
     cd cfg
     VER=$(git rev-parse --short HEAD)
     DEV=$(../udpmirror/getmacaddr)
-    curl -u device:device "http://localhost:8083/?setver=${DEV}&ver=ovbox-${VER}"
+    curl -u device:device "http://box.orlandoviols.com/?setver=${DEV}&ver=ovbox-${VER}"
     ../udpmirror/devconfigclient -l http://box.orlandoviols.com
 )
