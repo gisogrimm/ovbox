f019403f3b7d4113f6d6f59dd989e78fe54f7c9a
Giso Grimm <giso.grimm@vegri.net>
2020-08-31 20:06:01 +0200

    install ov-client instead of ovbox

--- installovbox.sh
+++ installovbox.sh
@@ -3,10 +3,12 @@
     # minimal error handling:
     trap "echo an error occured.;exit 1" ERR
 
+    export DEBIAN_FRONTEND=noninteractive
+
     # install dependencies:
-    sudo apt update
-    sudo apt upgrade --assume-yes
-    sudo apt install --assume-yes git zita-njbridge jackd2 liblo-dev nodejs libcurl4-openssl-dev build-essential libxml++2.6-dev libwebkit2gtk-4.0-dev libasound2-dev libboost-all-dev libcairomm-1.0-dev libeigen3-dev libfftw3-dev libfftw3-double3 libfftw3-single3 libgsl-dev libgtkmm-3.0-dev libgtksourceviewmm-3.0-dev libjack-jackd2-dev libltc-dev libmatio-dev libsndfile1-dev imagemagick libsamplerate0-dev
+    sudo -E apt update
+    sudo -E apt upgrade --assume-yes
+    sudo -E apt install --assume-yes git zita-njbridge jackd2 liblo-dev nodejs libcurl4-openssl-dev build-essential libxml++2.6-dev libwebkit2gtk-4.0-dev libasound2-dev libboost-all-dev libcairomm-1.0-dev libeigen3-dev libfftw3-dev libfftw3-double3 libfftw3-single3 libgsl-dev libgtkmm-3.0-dev libgtksourceviewmm-3.0-dev libjack-jackd2-dev libltc-dev libmatio-dev libsndfile1-dev imagemagick libsamplerate0-dev
 
     # clone and install TASCAR acoustic simulator repository:
     test -e tascar || git clone https://github.com/gisogrimm/tascar.git
@@ -21,7 +23,7 @@
 
     # get autorun file:
     rm -f autorun
-    wget https://github.com/gisogrimm/ovbox/raw/master/tools/pi/autorun
+    wget https://github.com/gisogrimm/ov-client/raw/master/tools/pi/autorun
     chmod a+x autorun
 
     # update real-time priority priviledges:
@@ -39,8 +41,8 @@
     sudo sed -i "s/127.0.1.1.*raspberry/127.0.1.1\tovbox/g" /etc/hosts
 
     # clone ovbox repo if not yet available:
-    sudo su -l ov -c "test -e ovbox || git clone http://github.com/gisogrimm/ovbox"
-    sudo su -l ov -c "make -C ovbox"
+    sudo su -l ov -c "test -e ov-client || git clone http://github.com/gisogrimm/ov-client"
+    sudo su -l ov -c "make -C ov-client"
 
     # activate overlay image to avoid damage of the SD card upon power off:
     sudo raspi-config nonint enable_overlayfs
