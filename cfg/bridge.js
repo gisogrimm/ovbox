55d8b4770bc26b579cad03ef420c8c5dce1d2c43
Giso Grimm <giso.grimm@vegri.net>
2020-09-02 11:33:14 +0200

    improve web mixer interface

--- cfg/bridge.js
+++ cfg/bridge.js
@@ -12,19 +12,12 @@ httpserver = http.createServer(function (req, res) {
     }
     catch(ee){
     }
-	
-    
-	
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<!DOCTYPE HTML>\n');
     res.write('<html><head><style>');
     res.write(hoscss);
-    res.write('</style><title>ORLANDOviols consort box</title>\n</head><body>\n');
+    res.write('</style><title>ORLANDOviols consort box mixer</title>\n</head><body>\n');
     res.write('<h1>ORLANDOviols consort box ('+devname+')</h1>\n<div id="mixer">mixer</div>\n');
-    //res.write('<div class="netctl" id="netctl"><p>Network settings</p>\n');
-    //res.write('<p><input type="checkbox" class="checkbox" id="peer2peer" name="peer2peer"><label for="peer2peer"> Peer-to-peer mode</label></p>\n');
-    //res.write('<p><input type="checkbox" class="checkbox" id="duplicates" name="duplicates"><label for="duplicates"> Send packages twice</label></p>\n');
-    //res.write('</div>');
     res.write('<script src="http://'+os.hostname()+':8080/socket.io/socket.io.js"></script>\n');
     res.write('<script>\n');
     res.write('var socket = io("http://'+os.hostname()+':8080");\n');
@@ -59,29 +52,11 @@ io.on('connection', function (socket) {
 	    if( msg[0].startsWith('/touchosc/level') ){
 		socket.emit('updatefader', msg[0], msg[1] );
 	    }
-	    if( msg[0] == '/peer2peer' ){
-		socket.emit('updatep2p', msg[1] );
-	    }
-	    if( msg[0] == '/duplicates' ){
-		socket.emit('updateduplicates', msg[1] );
-	    }
 	});
 	oscClient.send('/touchosc/connect',16);
     });
     socket.on('message', function (obj) {
 	oscClient.send(obj);
-    });
-    socket.on('peer2peer', function (obj) {
-	if( obj )
-	    oscClientMPLX.send( '/peer2peer',1);
-	else
-	    oscClientMPLX.send( '/peer2peer',0);
-    });
-    socket.on('duplicates', function (obj) {
-	if( obj )
-	    oscClientMPLX.send( '/duplicates',1);
-	else
-	    oscClientMPLX.send( '/duplicates',0);
     });
     socket.on('msg', function (obj) {
 	if( obj.hasOwnProperty('value') && (obj.value != null) ){
--- cfg/hos.js
+++ cfg/hos.js
@@ -14,61 +14,81 @@ socket.on("scene", function(scene){
 socket.on("newfader", function(faderno,val){
     fader="/touchosc/fader"+faderno;
     levelid="/touchosc/level"+faderno;
-    let el=document.getElementById("mixer");
-    let elp=document.createElement("p");
-    elp.setAttribute("class","mixerstrip");
-    let ell=document.createElement("label");
-    ell.setAttribute("for",fader);
-    ell.append(val);
-    let els=document.createElement("input");
-    els.setAttribute("class","fader");
-    els.setAttribute("type","range");
-    els.setAttribute("min","-20");
-    els.setAttribute("max","5");
-    els.setAttribute("value",val);
-    els.setAttribute("step","0.1");
-    els.setAttribute("id",fader);
-    let elsl=document.createElement("meter");
-    elsl.setAttribute("class","level");
-    elsl.setAttribute("min","0");
-    elsl.setAttribute("max","94");
-    elsl.setAttribute("low","71");
-    elsl.setAttribute("high","84");
-    elsl.setAttribute("optimum","54");
-    elsl.setAttribute("value",val);
-    elsl.setAttribute("id",levelid);
-    el.appendChild(elp);
-    elp.appendChild(els);
-    elp.appendChild(elsl);
-    elp.appendChild(ell);
+    let el_div = document.createElement("div");
+    let el_mixer=document.getElementById("mixer");
+    let classname = "mixerstrip";
+    if( val == "ego" )
+	classname = classname + " mixerego";
+    if( (val == "master") || (val == "reverb") )
+	classname = classname + " mixerother";
+    el_div.setAttribute("class",classname);
+    let el_lab=document.createElement("label");
+    el_lab.setAttribute("for",fader);
+    el_lab.append(val);
+    let el_fader=document.createElement("input");
+    el_fader.setAttribute("class","fader");
+    el_fader.setAttribute("type","range");
+    el_fader.setAttribute("min","-20");
+    el_fader.setAttribute("max","5");
+    el_fader.setAttribute("value",val);
+    el_fader.setAttribute("step","0.1");
+    el_fader.setAttribute("id",fader);
+    let el_gaintext=document.createElement("input");
+    el_gaintext.setAttribute("type","number");
+    el_gaintext.setAttribute("class","gaintxtfader");
+    el_gaintext.setAttribute("min","-20");
+    el_gaintext.setAttribute("max","5");
+    el_gaintext.setAttribute("value",val);
+    el_gaintext.setAttribute("step","0.1");
+    el_gaintext.setAttribute("id","txt"+fader);
+    let el_meter=document.createElement("meter");
+    el_meter.setAttribute("class","level");
+    el_meter.setAttribute("min","0");
+    el_meter.setAttribute("max","94");
+    el_meter.setAttribute("low","71");
+    el_meter.setAttribute("high","84");
+    el_meter.setAttribute("optimum","54");
+    el_meter.setAttribute("value",val);
+    el_meter.setAttribute("id",levelid);
+    let el_metertext=document.createElement("input");
+    el_metertext.setAttribute("type","text");
+    el_metertext.setAttribute("readonly","true");
+    el_metertext.setAttribute("class","gaintxtfader");
+    el_metertext.setAttribute("value",val);
+    el_metertext.setAttribute("id","txt"+levelid);
+    el_mixer.appendChild(el_div);
+    el_div.appendChild(el_lab);
+    el_div.appendChild(document.createElement("br"));
+    el_div.appendChild(el_fader);
+    el_div.appendChild(el_gaintext);
+    el_div.appendChild(document.createElement("br"));
+    el_div.appendChild(el_meter);
+    el_div.appendChild(el_metertext);
 });
 socket.on("updatefader", function(fader,val){
     let fad=document.getElementById(fader);
     if( fad!=null ){
 	fad.value=val;
     }
+    let fadt=document.getElementById("txt"+fader);
+    if( fadt!=null ){
+	fadt.value=val.toFixed(1);
+    }
 });
 let form = document.getElementById("mixer");
 form.oninput = handleChange;
 function handleChange(e) {
-    socket.emit("msg", { path: e.target.id, value: e.target.valueAsNumber } );
-}
-let netcf=document.getElementById("peer2peer");
-netcf.oninput = handleNetChange;
-function handleNetChange(e) {
-    socket.emit("peer2peer", e.target.checked);
-}
-let netdup = document.getElementById("duplicates");
-netdup.oninput = (function (e) { socket.emit("duplicates", e.target.checked);});
-socket.on("updatep2p", function(val){
-    let fad=document.getElementById("peer2peer");
-    if( fad!=null ){
-	fad.checked=(val>0);
+    if( e.target.id.substr(0,3)=="txt" ){
+	socket.emit("msg", { path: e.target.id.substr(3), value: e.target.valueAsNumber } );
+	let fad=document.getElementById(e.target.id.substr(3));
+	if( fad!=null ){
+	    fad.value=val;
+	}
+    }else{
+	socket.emit("msg", { path: e.target.id, value: e.target.valueAsNumber } );
+	let fadt=document.getElementById(e.target.id);
+	if( fadt!=null ){
+	    fadt.value=val.toFixed(1);
+	}
     }
-});
-socket.on("updateduplicates", function(val){
-    let fad=document.getElementById("duplicates");
-    if( fad!=null ){
-	fad.checked=(val>0);
-    }
-});
+}
