529c8201abc8e8820217a4df20a1140296631e94
Giso Grimm <giso.grimm@vegri.net>
2020-09-05 08:22:29 +0200

    clean repository

--- lobby/README.md
+++
@@ -1,4 +0,0 @@
-# The House of Consort lobby
-
-This is the user interface to manage the connections between the rooms
-and the devices.
--- lobby/start_local_php.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-(
-    cd www
-    ovboxuser=Giso php -S localhost:8081 &
-    ovboxuser=admin php -S localhost:8082 &
-    ovboxuser=device php -S localhost:8083 &
-    ovboxuser=room php -S localhost:8084 &
-)
